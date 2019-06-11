9700431dcd32ada6dcd104e9edf555b4c57c0cb0
Cara Takemoto <cktakemoto@gmail.com>
2019-06-11 16:07:37 -0400

    remove force true so not overwriting db

--- server/api/products.js
+++ server/api/products.js
@@ -5,6 +5,7 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
+    console.log(products)
     res.json(products)
   } catch (error) {
     next(error)
