2f4e57d7e24d317f12e73b43f5fb22099e959fab
Nikki Bergamini <nikkibergamini@gmail.com>
2019-06-11 12:08:29 -0400

    feature 17 done with only pulling cart from DB - breaking to research local storage

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,7 +4,7 @@ import history from '../history'
 /**
  * ACTION TYPES
  */
-
+const GET_CART = 'GET_CART'
 
 /**
  * INITIAL STATE
@@ -14,10 +14,26 @@ const defaultCart = {}
 /**
  * ACTION CREATORS
  */
+ export const getCartAction = (cart) => ({
+    type: GET_CART,
+    cart
+})
 
 /**
  * THUNK CREATORS
  */
+export const getCartThunk = () => {
+    return async(dispatch) => {
+        try {
+            const {data} = await axios.get("/api/cart") 
+            // only handles pulling cart from the DB for now
+            dispatch(getCartAction(data))
+        } catch (error) {
+            console.log("/api/cart get request failled")
+            //Need convo on error handling
+        }
+    }
+}
 
 /**
  * REDUCER
