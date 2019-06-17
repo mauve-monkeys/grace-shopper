6473b0e4562dfa36bf5070898d2cb184603e3e70
Kistina Gurung <youremail@example.com>
2019-06-16 20:40:55 -0400

    db-syned-but-not-showing-up

--- script/seed.js
+++ script/seed.js
@@ -1,86 +1,237 @@
 'use strict'
-const faker = require('faker')
+// const faker = require('faker')
 const db = require('../server/db')
+const Product = require('../server/db/models/product')
+const User = require('../server/db/models/user')
 
-function generateProducts(num) {
-  let productId = 1
-  const products = new Array(num).fill({}).map(product => {
-    const name = faker.commerce.productName()
-    const description = faker.commerce.productAdjective()
-    const imageUrl = 'https://loremflickr.com/320/240/dog'
-    const price = faker.commerce.price()
-    const SKU = ++productId
-
-    return {
-      name,
-      description,
-      imageUrl,
-      price,
-      SKU
-    }
+const products = [
+  Product.create({
+    name: 'Sparkling Wishbone Ring',
+    description:
+      'Make all your wishes come true with this delicate Pandora ring, crafted from sterling silver and lined with sparkling cubic zirconia stones. Simple yet elegant, this wishbone-shaped ring can be worn on its own as a shimmering statement, or paired with other Pandora stackable rings to create a unique, highly individual look.',
+    imageUrl:
+      'https://www.becharming.com/media/catalog/product/cache/1/image/750x750/9df78eab33525d08d6e5fb8d27136e95/b/e/beaded_wish_ring-196315.jpg',
+    price: 69.99,
+    SKU: 1,
+    stone: 'diamond',
+    band: 'sliver',
+    size: ''
+  }),
+  Product.create({
+    name: 'Flower Crown Ring',
+    description:
+      'Decorate fingers with this dazzling ring in Pandora Rose. Shaped as a flower garland, the blush-pink design sparkles with clear stones. Stack with similar designs or wear solo as a subtle statement.',
+    imageUrl:
+      'https://i.etsystatic.com/10202543/r/il/460163/1565583145/il_fullxfull.1565583145_5yie.jpg',
+    price: 39.99,
+    SKU: 2,
+    stone: 'ruby',
+    band: 'gold',
+    size: ''
+  }),
+  Product.create({
+    name: 'Princess Tiara Ring',
+    description:
+      'Fit for a modern fairy tale, this PANDORA Shine™ ring in 18K gold-plated sterling silver boasts a tiara with a heart and a brilliant cubic zirconia stone. It will make you feel like a princess every day.',
+    imageUrl:
+      'https://d1tvmw30728evr.cloudfront.net/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/b/k/bk909036.jpg',
+    price: 79.99,
+    SKU: 3,
+    stone: 'diamond',
+    band: 'sliver',
+    size: ''
+  }),
+  Product.create({
+    name: 'Heart of Winter Ring,',
+    description:
+      'Declare your love for the winter season with this chic sterling silver statement ring. Its embellished heart design is abstract and delicate at the same time, thanks to its shimmering cubic zirconia ice ruby motif.',
+    imageUrl: 'https://i.ebayimg.com/images/g/Tr4AAOSwMqBaXUZE/s-l300.jpg',
+    price: 49.99,
+    SKU: 4,
+    stone: 'diamond',
+    band: 'sliver',
+    size: ''
+  }),
+  Product.create({
+    name: 'Simple Band Ring',
+    description:
+      'Add a subtle touch of sparkle to every outfit with this Pandora eternity ring, crafted in polished Pandora Rose™ – a 14k rose gold-plated unique metal blend. Simple yet stylish, this classic style is embellished with shimmering cubic zirconia stones. Elegant on its own, it looks exceptional when stacked with other Pandora rings in contrasting metal tones.',
+    imageUrl:
+      'https://i03.hsncdn.com/is/image/HomeShoppingNetwork/prodfull/24ctw-white-diamond-14k-rose-gold-eternity-band-ring-d-00010101000000~8727726w.jpg',
+    price: 89.99,
+    SKU: 5,
+    stone: 'diamond',
+    band: 'gold',
+    size: ''
+  }),
+  Product.create({
+    name: 'Regal Beauty Ring',
+    description:
+      'Perfect for stacking, the royal-inspired pattern on this PANDORA Rose™ ring is unique and sophisticated. It turns a classic band into an eye-catching essential.',
+    imageUrl:
+      'https://soldes2016.club/wp-content/uploads/2018/11/jamie-joseph-rings-picture-of-full-of-regal-beauty-this-jamie-joseph-ring-is-fit-for-a-queen-the-of-jamie-joseph-rings-1.jpg',
+    price: 99.99,
+    SKU: 6,
+    stone: 'ruby',
+    band: 'rose',
+    size: ''
+  }),
+  Product.create({
+    name: 'Radiant Teardrop Ring',
+    description:
+      'This ultra-glamorous statement ring in PANDORA Rose™ will cast glorious light on your fingers, courtesy of its beautiful teardrop-shaped cubic zirconia stone.',
+    imageUrl:
+      'https://www.reeds.com/pub/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/p/a/pandora_rose_radiant_teardrop_ring_clear_cubic_zirconia-1-186251cz-t1559105710.jpg',
+    price: 99.99,
+    SKU: 7,
+    stone: 'diamond',
+    band: 'gold',
+    size: ''
+  }),
+  Product.create({
+    name: 'Dazzling Droplet Ring',
+    description:
+      'Nothing less than absolutely dazzling, this sterling silver ring features pavé-set stones on a central dome. Reflecting the beautiful light effect of a water droplet, this striking ring looks best when stacked with the other rings in the Droplets series.',
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/5123oDEdRcL._UL1000_.jpg',
+    price: 89.99,
+    SKU: 8,
+    stone: 'diamond',
+    band: 'sliver',
+    size: ''
+  }),
+  Product.create({
+    name: ' Sparkling Red Heart Ring',
+    description:
+      "Set your heart on fire with this sparkling Pandora heart ring. Crafted in sterling silver, this stunning solitaire ring features a heart-shaped cubic zirconia stone in ruby red, flanked by two shimmering stones. A beautiful gift for Valentine's Day, birthdays, and anniversaries, it is the perfect expression of love. Let it shine solo or stack it with other Pandora rings.",
+    imageUrl:
+      'https://img.shopstyle-cdn.com/pim/b0/e3/b0e3825ec859df87ffed7a5b367f6e47_xlarge.jpg',
+    price: 59.99,
+    SKU: 9,
+    stone: 'ruby',
+    band: 'sliver',
+    size: ''
+  }),
+  Product.create({
+    name: 'Sparkling Elegance Ring',
+    description:
+      'A trio of striking stones decorates this elegant ring in Pandora Rose. The blush-pink design has a vintage feel, and is perfect for wear alone or in ring stacks.',
+    imageUrl:
+      'https://www.becharming.com/media/catalog/product/cache/1/image/750x750/9df78eab33525d08d6e5fb8d27136e95/r/a/radiant_elegance_ring_rose-180986cz.jpg',
+    price: 69.99,
+    SKU: 10,
+    stone: 'diamond',
+    band: 'gold',
+    size: ''
+  }),
+  Product.create({
+    name: 'Hearts of Pandora Ring',
+    description:
+      'Show that your love is forever with this classic Pandora Shine eternity ring in 18K gold-plated sterling silver. Its golden color, cut-out hearts and clear cubic zirconia stones make it a fabulous stacking ring.',
+    imageUrl: 'https://www.denmarkstyle.net/images/enlarged/180963CZ.jpg',
+    price: 59.99,
+    SKU: 11,
+    stone: 'diamond',
+    band: 'gold',
+    size: ''
+  }),
+  Product.create({
+    name: 'Twist Of Fate Stackable Ring',
+    description:
+      'Two rows of clear sparkling cubic zirconias entwine in this sterling silver ring, representing how your lives are connected. Beautiful alone and also stacked with your favorite PANDORA rings.',
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/61hWKbujoJL._UY395_.jpg',
+    price: 69.99,
+    SKU: 12,
+    stone: 'diamond',
+    band: 'sliver',
+    size: ''
   })
-  return products
-}
+]
 
-function generateUsers(num) {
-  const users = new Array(num).fill({}).map(user => {
-    const firstName = faker.name.firstName()
-    const lastName = faker.name.lastName()
-    const phoneNumber = faker.phone.phoneNumber()
-    const shippingAddress = `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}, ${faker.address.zipCode()}`
-    const billingAddress = `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}, ${faker.address.zipCode()}`
-    return {
-      firstName,
-      lastName,
-      phoneNumber,
-      shippingAddress,
-      billingAddress,
-      email: `${firstName}.${lastName}@example.com`,
-      password: '1234',
-      username: `${firstName}${lastName[0]}`
-    }
+const users = [
+  User.create({
+    username: 'cara.t',
+    email: 'cara.t@gmail.com',
+    password: 'kitty',
+    googleId: '',
+    firstName: 'Cara',
+    lastName: 'Takemoto',
+    billingAddress: '123 terrace st, New York, NY 12345',
+    shippingAddress: '123 terrace st, New York, NY 12345',
+    phoneNumber: '666'
+  }),
+  User.create({
+    username: 'maddie.e',
+    email: 'maddie.e@gmail.com',
+    password: 'lab',
+    googleId: '',
+    firstName: 'Madeline',
+    lastName: 'Emde',
+    billingAddress: '6789 prince st, Orlando, FL 58364',
+    shippingAddress: '6789 prince st, Orlando, FL 58364',
+    phoneNumber: '1275983023'
+  }),
+  User.create({
+    username: 'nikki.b',
+    email: 'nikki.b@gmail.com',
+    password: 'pitbull',
+    googleId: '',
+    firstName: 'Nikki',
+    lastName: 'Bergamini',
+    billingAddress: '235 grey st, Nevada, Las Vegas 3467',
+    shippingAddress: '634 yellow st, Nevada, Las Vegas 2457',
+    phoneNumber: ''
+  }),
+  User.create({
+    username: 'kistina.g',
+    email: 'kistina.g@gmail.com',
+    password: 'goldendoddle',
+    googleId: '',
+    firstName: 'Kistina',
+    lastName: 'Gurung',
+    billingAddress: '726 bowery drive, Palisades Park, NJ 2563',
+    shippingAddress: '2353 canal st, Leonia, New Jersey 778',
+    phoneNumber: ''
   })
-  return users
-}
+]
 
-async function createOrders(num, products, users) {
-  // const orders = await db.models.order.bulkCreate(new Array(num).fill({}))
-  // let randInt = Math.floor(Math.random() * products.length)
-  // await orders.addProduct(products[randInt])
-  const order = await db.models.order.create({})
-  await order.addProduct(products[0])
-  await order.addProduct(products[1])
-  await users[0].addOrder(order)
+// async function createOrders(num, products, users) {
+//   // const orders = await db.models.order.bulkCreate(new Array(num).fill({}))
+//   // let randInt = Math.floor(Math.random() * products.length)
+//   // await orders.addProduct(products[randInt])
+//   const order = await db.models.order.create({})
+//   await order.addProduct(products[0])
+//   await order.addProduct(products[1])
+//   await users[0].addOrder(order)
 
-  const order2 = await db.models.order.create({
-    status: 'confirmed'
-  })
-  await order2.addProduct(products[1])
-  await order2.addProduct(products[2])
-  await users[1].addOrder(order2)
-}
+//   const order2 = await db.models.order.create({
+//     status: 'confirmed'
+//   })
+//   await order2.addProduct(products[1])
+//   await order2.addProduct(products[2])
+//   await users[1].addOrder(order2)
+// }
 
-async function seed(numProducts, numUsers) {
+async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const userData = generateUsers(numUsers)
-  const users = []
+  const usersData = []
 
-  for (let i = 0; i < userData.length; i++) {
-    let user = await db.models.user.create(userData[i])
+  for (let i = 0; i < usersData.length; i++) {
+    let user = await db.models.user.create(usersData[i])
     users.push(user)
   }
 
-  const productData = generateProducts(numProducts)
-  const products = []
+  const productsData = []
 
-  for (let i = 0; i < productData.length; i++) {
-    let product = await db.models.product.create(productData[i])
+  for (let i = 0; i < productsData.length; i++) {
+    let product = await db.models.product.create(productsData[i])
     products.push(product)
   }
 
-  await createOrders(2, products, users)
+  // await createOrders(2, products, users)
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${products.length} products`)
