d17e2360a8f23916a6fb726cdd3b9e991bed7917
Cara Takemoto <cktakemoto@gmail.com>
2019-06-17 22:25:09 -0400

    fixed welcome page positioning

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
     <div className="welcome-page">
       <img src="/welcomebanner.jpg" />
       <div>
-        <h1>Weclome to Grace Rings</h1>
+        <h1>Welcome to Grace Rings</h1>
         <p>Why not spend your money here?</p>
       </div>
     </div>
