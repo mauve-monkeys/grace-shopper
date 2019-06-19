08765b15cda9129750d8d022ed39ef0d05518188
Cara Takemoto <cktakemoto@gmail.com>
2019-06-18 23:38:06 -0400

    add form styles to edit profile form

--- client/components/profileForm.js
+++ client/components/profileForm.js
@@ -21,44 +21,67 @@ class ProfileForm extends React.Component {
       return <div />
     }
     return (
-      <form onSubmit={this.handleSumbit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          name="firstName"
-          value={this.state.firstName}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          name="lastName"
-          value={this.state.lastName}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="shippingAddress">Shipping Address:</label>
-        <input
-          type="text"
-          name="shippingAddress"
-          value={this.state.shippingAddress}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="billingAddress">Billing Address:</label>
-        <input
-          type="text"
-          name="billingAddress"
-          value={this.state.billingAddress}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="form-container">
+        <h3 className="form-title">Edit Profile</h3>
+        <form onSubmit={this.handleSumbit}>
+          <div className="form-field-container">
+            <div className="form-field">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-field-container">
+            <div className="form-field">
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-field-container">
+            <div className="form-field">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-field-container">
+            <div className="form-field">
+              <label htmlFor="shippingAddress">Shipping Address:</label>
+              <input
+                type="text"
+                name="shippingAddress"
+                value={this.state.shippingAddress}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-field-container">
+            <div className="form-field">
+              <label htmlFor="billingAddress">Billing Address:</label>
+              <input
+                type="text"
+                name="billingAddress"
+                value={this.state.billingAddress}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     )
   }
 
